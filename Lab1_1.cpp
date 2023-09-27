1e7f0f0d35290471079cc6b3d813f8da8b6a0012
mmchruscinski <mmchruscinski@gmail.com>
2023-09-27 16:52:15 +0200

    Update Lab1_1.cpp

--- Lab1_1.cpp
+++ Lab1_1.cpp
@@ -1,7 +1,6 @@
 //Write a program that will act as a guessing game in which the user has eight tries to
 //guess a randomly generated number. The program will tell the user each time whether
 //he guessed high or low.
-//test
 
 #include <iostream>
 #include <stdlib.h>
