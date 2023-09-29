918aba7a16e1ce9153b5c8be3aa8edbfc1a15b17
mmchruscinski <mmchruscinski@gmail.com>
2023-09-29 15:39:15 +0200

    Create Lab3_1.cpp

---
+++ Lab3_1.cpp
@@ -0,0 +1,13 @@
+//Design, implement, and test a class which represents a rational number . The 𝑛/𝑚 , 𝑚 ≠ 0
+//class should provide methods which implement simple arithmetic operations on rational
+//numbers (e. g. addition, subtraction, multiplication, division). Implement addition and
+//subtraction as standard member methods and multiplication as well as division as friend
+//functions. In the main() function write the code that demonstrates the use of the class.
+
+#include <iostream>
+using namespace std;
+
+
+
+
+
